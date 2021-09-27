83dec7b442279aceccd95e5fed1a225f2fd3064e
AntipasIC <iantipas@yahoo.com>
2021-09-27 18:01:39 +0200

    Initial Commit

---
+++ posts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render 
+from .models import Post,Category
+import random
+
+def index(request):
+    featuredPost = Post.objects.filter(featured=True)
+    index = randnum()
+    allCategories = Category.objects.all()
+    categorisedPost = Post.objects.filter(Category=allCategories[index])
+    context = {
+        'post_list': featuredPost,
+        'cat_list': allCategories,
+        'catpost_list':categorisedPost
+
+    }
+    return render(request,'index.html',context)
+
+def randnum():
+    return random.randint(0,2)
+ 
+def blog(request):
+    featuredPost = Post.objects.filter(featured=True)
+    index = randnum()
+    allCategories = Category.objects.all()
+    categorisedPost = Post.objects.filter(Category=allCategories[index])
+    context = {
+        'post_list': featuredPost,
+        'cat_list': allCategories,
+        'catpost_list':categorisedPost
+
+    }
+    return render(request,'blog.html',context)
