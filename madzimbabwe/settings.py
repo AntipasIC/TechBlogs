8d003d4eaa4886565eadcf2df70cab507f78f81b
AntipasIC <iantipas@yahoo.com>
2021-09-28 14:17:48 +0200

    Initial Commit

--- madzimbabwe/settings.py
+++ madzimbabwe/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tx4zw9u3txndukw!1*3j(pd1oie4ep27szlkl+5bhlm-jfaz3_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["madzimbabwe.herokuapp.com/"]
 
