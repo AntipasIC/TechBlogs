4a0cc834370f54ec6b1acb715a4b635d83054496
AntipasIC <iantipas@yahoo.com>
2021-09-28 14:49:31 +0200

    Initial Commit

--- madzimbabwe/settings.py
+++ madzimbabwe/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -113,8 +113,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static_in_env'),]
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 MEDIA_ROOT = os.path.join(VENV_PATH,'media_root')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
-prod_db=database_url.config(conn_max_age=500)
+prod_db=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
